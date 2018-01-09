b86b54e568091bd6a51888cf516043df5f3ef3be
Aubrey Neimeier <neimeier.1@osu.edu>
2018-01-09 16:13:17 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,6 +6,10 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+end
+
+  def brand= (brand)
+    @brand = brand
     BRANDS.each do |brand|
       if !@brand == brand || BRANDS.count == 0
         BRANDS << brand
