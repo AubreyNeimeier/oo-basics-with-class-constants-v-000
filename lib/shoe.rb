07cbe6b93a3ea1e89dfd14f02438e15205102689
Aubrey Neimeier <neimeier.1@osu.edu>
2018-01-09 16:43:48 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,12 +6,9 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-  end
-
-  def brands= (brand)
-    @brand = brand
     BRANDS << brand
   end
+
 
   def cobble
     self.condition = "new"
